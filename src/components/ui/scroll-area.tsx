35b56127311ae4c2956481417c8eccd720d0d391
Samar Kumar <samarkumr1313@gmail.com>
2025-05-29 09:01:53 +0000

    highlight scroll button in (content suggestion) box

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -49,7 +49,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-primary opacity-75" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
